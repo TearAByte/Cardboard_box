a33a3b401f82bdbe124a9c8aa359cb804a37c57f
Martin Babael <babaelmartin@gmail.com>
2023-09-29 16:19:44 +0800

    cam changes

--- Cardboard Box/Assets/Scripts/Input System/CameraStatic.cs
+++ Cardboard Box/Assets/Scripts/Input System/CameraStatic.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class CameraStatic : MonoBehaviour{
     [SerializeField]
-    private GameObject camPosition;
+    private GameObject camPosition, halfwayPoint;
 
     // Update is called once per frame
-    void LateUpdate()
-    {
+    private void Update(){
+        Physics.OverlapBox(halfwayPoint.transform.position, new Vector3(1, 8, 6));
+    }
+    void LateUpdate(){
         transform.position = camPosition.transform.position;
+    }
+
+    void OnDrawGizmosSelected(){
+        // Draw a semitransparent red cube at the transforms position
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(halfwayPoint.transform.position, new Vector3(1, 8, 6));
     }
 }
